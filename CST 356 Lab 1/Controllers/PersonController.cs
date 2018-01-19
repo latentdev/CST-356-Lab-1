4fef888fc461d3e704cb574b1dd461aa7ab68825
Nicholas Olson <latentdev@gmail.com>
2018-01-18 22:06:56 -0800

    Initial Commit
    Upload of cod for Lab 1

---
+++ CST 356 Lab 1/Controllers/PersonController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CST_356_Lab_1.Controllers
+{
+    public class PersonController : Controller
+    {
+        // GET: Person
+        public ActionResult Index()
+        {
+            var person = new Models.Person { Name = "Nick Olson", Age = 30, BirthDate = new DateTime(1987, 9, 6) };
+
+            return View(person);
+        }
+    }
+}
---
+++ CST 356 Lab 1/Models/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CST_356_Lab_1.Models
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
